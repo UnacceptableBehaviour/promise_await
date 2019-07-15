74462f912cb0c190f7d8074181a531d935ee70e3
Simon <simon@ferdiesfoodlab.co.uk>
2019-07-15 18:04:59 +0100

    git status using JS & fetch

--- README.md
+++ README.md
@@ -2,22 +2,51 @@
 ## Java Script - getting to grips with asynchronous idioms in  JS
 ### Tutorials from Travery Media (https://twitter.com/traversymedia)
 
-## Basic flask setup notes (webserver)
-https://github.com/UnacceptableBehaviour/00_flask/blob/master/README.md
+
+## Creating a small web tool that reports the status of all the repos:
+### Steps:
+Access GIT using Git API in JS
+Pass data from JS to Python
+Interrogate local repos
+Pass data back from Python to JS
+Use JS to display the results
+
+test code:
+```
+@app.route('/js_fetch_test', methods=["GET", "POST"])
+js_fetch_both_ways_example.js                           # Click JS fetch follow instructions
+```
+
+NEXT:
+See how may anit patterns we've trodden in:
+Promises & their anti patterns (https://www.nearform.com/blog/javascript-promises-the-definitive-guid)
+
+Combine ‘Get Data’ & ‘Git Status’ functionality into one button - await?
+
+Add a button to each repo that allows display of the Readme.md or Context.md file
+
+Create context.md layout template based on FTD doc.
 
 
 
-
-
-
-REFERENCES
-Asynch JS Crash Course (vid 24m) Callbacks, Promises, Async Await
+## REFERENCES
+Asynch JS Crash Course (vid 24m) Callbacks, Promises, Async Await Traversy Media (https://twitter.com/traversymedia)
 https://www.youtube.com/watch?v=PoRJizFvM7s&list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX&index=11
 
-AutoComplete using a JSON file (vid 19m)
+AutoComplete using a JSON file (vid 19m) Traversy Media (https://twitter.com/traversymedia)
 https://www.youtube.com/watch?v=1iysNUrI3lw&list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX&index=15
 
-Ajax CrashC (vid 1h9)
+Ajax CrashC (vid 1h9) Traversy Media (https://twitter.com/traversymedia)
 https://www.youtube.com/watch?v=82hnvUYY6QA&list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX&index=12
 
+From python to JS and back
+https://dev.to/healeycodes/talking-to-python-from-javascript-and-back-again-31jf
 
+Mozilla Docs - Fetch API
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+
+Fetch API - 31m vid Traversy Media (https://twitter.com/traversymedia)
+https://www.youtube.com/watch?v=Oive66jrwBs&list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX&index=13
+
+Postman
+https://www.getpostman.com/
--- static/js_fetch_both_ways_example.js
+++ static/js_fetch_both_ways_example.js
@@ -69,7 +69,7 @@ function create_repo_report_file_list_card(repo_name_key, card_key_as_title, fil
 
   } catch (error) { console.log(`ERROR: ${error.name}`); }
   
-  var card_element_as_html = `<div class='card'>
+  var card_element_as_html = `<div class='card ${card_key_as_title}'>
                                 <ul id="${repo_name_key}_ul_${card_key_as_title}">
                                   <li>${card_key_as_title.toUpperCase()}: ${file_list.length}</li>
                                   ${file_list_as_html}
@@ -91,20 +91,20 @@ function create_repo_report_output_section(repo_name_key, repo_data){
   //                            untracked: ["antenna_physics.txt"]    }
   
   // opening html - backround colour: WARNING
-  var output_section_html = `<div id="repo-output" style="background-color:${COLOUR_WARNING}">
+  var output_section_html = `<div id="repo-output">
         <div id="${repo_name_key}" class="repo-output-warn">`;
   
   
   file_lists = repo_data[repo_name_key];
   delete file_lists['desc'];
   
-  // DIFFERENT WYS TO DO THIS LOOP
+  // DIFFERENT WAYS TO DO THIS LOOP
   // bench: https://hackernoon.com/5-techniques-to-iterate-over-javascript-object-entries-and-their-performance-6602dcb708a8
   //
   // could use an array of keys to control the order if necessary
   // loop through arrays - skip description (desc)
   //for (let [card_key_as_title, file_list] of Object.entries(file_lists)) {    // TICK - WORK
-  //  console.log('for (let [key, file_list] of Object.entries(file_lists))')
+  //  console.log('for (let [key, file_list] of Object.entries(file_lists))');
   //  console.log(card_key_as_title);
   //  console.log(file_list);
   //  console.log('...........');
@@ -113,7 +113,7 @@ function create_repo_report_output_section(repo_name_key, repo_data){
   // - - 
   //Object.entries(repo_data).forEach(                // NEEDS WORK
   //    ([card_key_as_title, file_list]) => {
-  //        console.log('Object.entries(repo_data).forEach(')
+  //        console.log('Object.entries(repo_data).forEach(');
   //        console.log(card_key_as_title);
   //        console.log(file_list);
   //        console.log('...........');
@@ -121,14 +121,30 @@ function create_repo_report_output_section(repo_name_key, repo_data){
   //        }
   //);
   // recode using for in - just want to see if above works  
-  for (var card_key_as_title in file_lists) {
-    file_list = file_lists[card_key_as_title];
-    console.log('for (var card_key_as_title in file_lists)')
-    console.log(card_key_as_title);
-    console.log(file_list);
-    console.log('...........');    
-    output_section_html += create_repo_report_file_list_card(repo_name_key, card_key_as_title, file_list);  
+  //for (var card_key_as_title in file_lists) {
+  //  file_list = file_lists[card_key_as_title];
+  //  console.log('for (var card_key_as_title in file_lists)');
+  //  console.log(card_key_as_title);
+  //  console.log(file_list);
+  //  console.log('...........');    
+  //  output_section_html += create_repo_report_file_list_card(repo_name_key, card_key_as_title, file_list);  
+  //}
+  // recode using for in - and an array to create specific order
+  var order_of_lists = [ 'changes_to_commit', 'not_staged', 'untracked'];
+  
+  for (var index in order_of_lists) {   // thought this would yiel strings, but it gives numbers
+    card_key_as_title = order_of_lists[index];
+    
+    if (card_key_as_title in file_lists) {          // make sure we're not firing blank list over
+      file_list = file_lists[card_key_as_title];
+      console.log('ARRAY for (var card_key_as_title in order_of_lists)');
+      console.log(card_key_as_title);
+      console.log(file_list);
+      console.log('...........');     
+      output_section_html += create_repo_report_file_list_card(repo_name_key, card_key_as_title, file_list);        
+    }
   }
+
   
   // closing html
   output_section_html +=  `</div></div>`;
@@ -166,8 +182,6 @@ function create_filled_in_status_element(repo_name_key, repo_data){
   // is is a GREEN card or a RED card?  
   var output_section_html = '';
   
-  //create_repo_report_output_section(repo_name_key, repo_data);
-  
   // if (repo_data[repo_name_key].keys > 1) {  NO WORK
   if (Object.keys(repo_data[repo_name_key]).length > 1) {  // we have file lists  
     // create WARNING output
@@ -175,7 +189,7 @@ function create_filled_in_status_element(repo_name_key, repo_data){
   
   } else {
     // create OK output
-    output_section_html = `<div id="repo-output" class="repo-output-good" style="background-color:${COLOUR_WE_ARE_GOOD}">Repo: ${repo_name_key} is up to date.</div>`;
+    output_section_html = `<div id="repo-output" class="repo-output-good">Repo: ${repo_name_key} is up to date.</div>`;
   }
   
   
@@ -336,7 +350,7 @@ function fetchButtonPOST() {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function fetchButtonGitStatus(){
   
-  outputDiv.innerHTML = '1s click GET (blue), then POST (green), the GitStatus';
+  outputDiv.innerHTML = '1s click GET (blue), then GitStatus button';
 
   // get local repo status info
   fetch( '/js_fetch_test', {
